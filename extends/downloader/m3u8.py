11e662f863cae79a0bb9375b2931f765922c1b9f
niugang <niugang.dev@foxmail.com>
2019-06-29 17:43:54 +0800

    修改下载方法 降低对磁盘损伤

--- extends/downloader/m3u8.py
+++ extends/downloader/m3u8.py
@@ -6,7 +6,8 @@ import threading
 import os
 import json
 import ssl
-from extends.db.sql import Task
+import platform
+# from extends.db.sql import Task
 from Crypto.Cipher import AES
 import datetime
 from time import sleep
@@ -26,6 +27,9 @@ class m3u8(interface):
         self.urls: list = []
         self.isPause: bool = False
         self.isStop: bool = False
+        self.slice = 100
+        self.memory = {}
+        self.sysstr = platform.system()
         super(m3u8, self).__init__(uri, out_path, filename, manage, gid)
         # self.db_task = Task()
 
@@ -38,8 +42,8 @@ class m3u8(interface):
         #     {'task_id': self.gid, 'url': self.uri, 'filename': self.filename, 'file_path': self.out_path,
         #      'user_data': '', 'status': self.status, 'type': 'm3u8'})
         # ssl._create_default_https_context = ssl._create_unverified_context
-        self.tmp_path = "%s/%s" % (os.path.join(Config.getInstance().APP_PATH, self.manage.getConfig('tmp_path')),
-                                   datetime.datetime.now().strftime('%Y%m%d_%H%M%S'))
+        (self.tmp_path,) = os.path.join(Config.getInstance().APP_PATH, self.manage.getConfig('tmp_path'),
+                                        datetime.datetime.now().strftime('%Y%m%d_%H%M%S')),
         self.setStatus(1)
         all_content = requests.get(self.uri, verify=False, timeout=(3, 7)).text  # 获取第一层M3U8文件内容
         if "#EXTM3U" not in all_content:
@@ -119,7 +123,7 @@ class m3u8(interface):
                         url = self.urlsbox.pop(0)
                         index = self.urls.index(url)
                         t = threading.Thread(target=self.down,
-                                             args=(url, '%s/%s.ts' % (self.tmp_path, index)))
+                                             args=(url, self.tmp_path, index))
                         t.setDaemon(True)
                         t.start()
                         self.thread_total += 1
@@ -127,16 +131,20 @@ class m3u8(interface):
                     break
                 sleep(0.001)
             if not self.isStop:
+                while True:
+                    if self.thread_total == 0 and len(self.memory) == 0:
+                        break
+                    sleep(0.001)
                 self.complate()
             else:
                 while True:
-                    if self.thread_total == 0:
+                    if self.thread_total == 0 and len(self.memory) == 0:
                         break
                     sleep(0.001)
                 self.clean()
 
     def __del__(self):
-        if self.isStop != True:
+        if not self.isStop:
             tmps = "%s/%s.json" % (
                 os.path.join(Config.getInstance().APP_PATH, self.manage.getConfig('tmp_path')), self.gid)
             if os.path.exists(tmps):
@@ -150,41 +158,66 @@ class m3u8(interface):
                 self.urlsbox = json.load(fp)
 
     def clean(self):
-        os.system("rm -rf %s" % self.tmp_path)
+        if self.sysstr == 'Windows':
+            os.system("rmdir /s /q %s" % self.tmp_path)
+        else:
+            os.system("rm -rf %s" % self.tmp_path)
 
     def complate(self):
-        file_list = os.listdir(self.tmp_path)
-        file_list.sort(key=lambda x: int(x[:-3]))
-        file_path = os.path.join(self.out_path, self.filename)
-        if not os.path.exists(self.out_path):
-            os.makedirs(self.out_path)
-        with open(file_path, 'wb+') as fhand:
-            for i in range(len(file_list)):
-                fhand.write(open(os.path.join(self.tmp_path, file_list[i]), 'rb').read())
-        os.system("rm -rf %s" % self.tmp_path)
+        if self.sysstr == 'Windows':
+            file_path = os.path.join(self.out_path, self.filename)
+            os.system('copy /b %s\*.ts %s' % (self.tmp_path, file_path))
+            os.system("rmdir /s /q %s" % self.tmp_path)
+        else:
+            file_list = os.listdir(self.tmp_path)
+            file_list.sort(key=lambda x: int(x[:-3]))
+            file_path = os.path.join(self.out_path, self.filename)
+            if not os.path.exists(self.out_path):
+                os.makedirs(self.out_path)
+            with open(file_path, 'wb+') as fhand:
+                for i in range(len(file_list)):
+                    fhand.write(open(os.path.join(self.tmp_path, file_list[i]), 'rb').read())
+            os.system("rm -rf %s" % self.tmp_path)
         self.setStatus(6)
 
-    def down(self, url, tmpname):
+    def writeContent(self, index, tmp_path, content):
+        key = index // self.slice
+        key2 = index % self.slice
+        max_key = (len(self.urls) - 1) // self.slice
+        max_key2 = (len(self.urls) - 1) % self.slice
+        if key not in self.memory:
+            self.memory[key] = {}
+        self.memory[key][key2] = content
+        if key == max_key:
+            if len(self.memory[key]) == max_key2 + 1:
+                with open(os.path.join("%s/%06d.ts" % (tmp_path, key)), 'ab+') as f:
+                    for x in range(max_key2 + 1):
+                        f.write(self.memory[key].get(x))
+                    del self.memory[key]
+        elif len(self.memory[key]) == self.slice:
+            with open(os.path.join("%s/%06d.ts" % (tmp_path, key)), 'ab+') as f:
+                for x in range(self.slice):
+                    f.write(self.memory[key].get(x))
+                del self.memory[key]
+
+    def down(self, url, tmp_path, index):
         try:
             res = requests.get(url, verify=False, timeout=(3, 7))
             if len(self.key):
                 cryptor = AES.new(self.key, AES.MODE_CBC, self.key)
                 content = cryptor.decrypt(res.content)
                 if len(content):
-                    with open(os.path.join(tmpname), 'ab') as f:
-                        f.write(content)
-                        f.flush()
+                    self.writeContent(index, tmp_path, content)
                 else:
                     self.urlsbox.append(url)
             else:
                 if len(res.content):
-                    with open(os.path.join(tmpname), 'ab') as f:
-                        f.write(res.content)
-                        f.flush()
+                    self.writeContent(index, tmp_path, res.content)
                 else:
                     self.urlsbox.append(url)
 
         except Exception as e:
+            print(e)
             self.urlsbox.append(url)
 
         self.thread_total -= 1
@@ -226,4 +259,4 @@ class m3u8(interface):
 
 
 if __name__ == '__main__':
-    d = m3u8("", "../down/",'', None)
+    d = m3u8("", "../down/", '', None)
